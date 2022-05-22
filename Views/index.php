27f158de29067e6e5aabcc91fc46d9f6bccb296a
Cu34v0 <conradoeme@gmail.com>
2022-05-22 13:02:04 -0500

    login

--- Views/index.php
+++ Views/index.php
@@ -21,7 +21,7 @@
                         <div class="col-lg-5">
                             <div class="card shadow-lg border-0 rounded-lg mt-5">
                                 <div class="card-header">
-                                    <h3 class="text-center font-weight-light my-4"> <i class="fas fa-box mx-1"></i> Sistema de Administración</h3>
+                                    <h3 class="text-center font-weight-light my-4"> <i class="fas fa-toolbox mx-1"></i> Sistema de Administración</h3>
                                 </div>
                                 <div class="card-body">
                                     <form id="frmLogin">
